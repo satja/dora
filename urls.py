10582c652e90e037383e82effa1879d91c4d0892
satja <askurdija@gmail.com>
2014-12-27 18:16:44 +0100

    undo forgot password

--- urls.py
+++ urls.py
@@ -21,16 +21,6 @@ urlpatterns = patterns(
 
 urlpatterns += staticfiles_urlpatterns()
 
-patterns += (
-    url(r'^passreset/$', auth_views.password_reset, name='forgot_password1'),
-    url(r'^passresetdone/$',
-        auth_views.password_reset_done, name='forgot_password2'),
-    url(r'^passresetconfirm/(?P<uidb36>[-\w]+)/(?P<token>[-\w]+)/$',
-        auth_views.password_reset_confirm, name='forgot_password3'),
-    url(r'^passresetcomplete/$',
-        auth_views.password_reset_complete, name='forgot_password4'),
-)
-
 # Iz http://stackoverflow.com/questions/5517950/django-media-url-and-media-root
 if settings.DEBUG:
     urlpatterns += patterns(
